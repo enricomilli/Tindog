e8be6510e633d0f9bb31867203ab6fc6d0ce2967
enricomilli <125762863+enricomilli@users.noreply.github.com>
2023-06-04 10:20:18 +0200

    images

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 export const dogs = [
     {
         name: "Rex",
-        avatar: "images/dog-rex.jpg",
+        avatar: "/images/dog-rex.jpg",
         age: 25,
         bio: "Art. Literature. Natural wine. Yoga.",
         hasBeenSwiped: false,
         hasBeenLiked: false
     },{
         name: "Bella",
-        avatar: "images/dog-bella.jpg",
+        avatar: "/images/dog-bella.jpg",
         age: 43,
         bio: "Yup, that's my owner. U can meet him if you want",
         hasBeenSwiped: false,
@@ -16,12 +16,10 @@ export const dogs = [
     },
     {
         name: "Teddy",
-        avatar: "images/dog-teddy.jpg",
+        avatar: "/images/dog-teddy.jpg",
         age: 30,
         bio: "How you doin?",
         hasBeenSwiped: false,
         hasBeenLiked: false
     }
 ]
-
-// Remember to export the data!
--- index.js
+++ index.js
@@ -55,7 +55,7 @@ noBtn.addEventListener('click', ()=>{
         setTimeout(()=> {
         nopeImg.style.display = 'none'
         profiles.innerHTML = `
-            <img src='/images/4791941-200.png' alt='People-nearby-graphic' id='error-msg-graphic'>
+            <img src="/images/4791941-200.png" alt="People-nearby-graphic" id="error-msg-graphic">
             <h1 class='error-message'>Sorry, no more profiles nearby.</h1>
         `}, 1500)
     }   
